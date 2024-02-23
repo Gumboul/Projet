c977c436651502c94e445495eeec10044e69114a
Gumboul <160596360+Gumboul@users.noreply.github.com>
2024-02-23 12:11:27 +0100

    updtate

--- public class Personnages.cs
+++ public class Personnages.cs
@@ -11,11 +11,10 @@ public class Personnages
     }
     public static Personnages
     {
-        joueur.zone(Lieu);
+        joueur.zone(Lieu); 
     }
     public static Personnages
     {
         joueur.fouille(Zone);
     }
-    
 }
