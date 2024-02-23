1894ca85947de901dfdc138d6dc704353d9e35d4
Gumboul <160596360+Gumboul@users.noreply.github.com>
2024-02-23 11:50:45 +0100

    V1

---
+++ public class Personnages.cs
@@ -0,0 +1,13 @@
+public class Personnages
+{
+    private string lieu 
+    private int degat
+    private int nombre_pv
+    public Personnages (string lieu2, int degats2, int nombre_pv2) 
+    {
+        this.lieu = lieu2;
+        this.degats = degats2;
+        this.nombre_pv = nombre_pv2;
+    }
+    
+}
